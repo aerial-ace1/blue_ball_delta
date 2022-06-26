580c1aa00ae05b8db64570f93fc6b33b4e48b15d
aerial-ace1 <96421655+aerial-ace1@users.noreply.github.com>
2022-06-27 00:40:52 +0530

    base done

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 const platform_list = [];
+var platform_generator;
+var hearts;
+var play_but = document.getElementById('play');
 
 
 canvas.width = 700;
 canvas.height = 300;
 
 
-const mouse = {
-  x: innerWidth / 2,
-  y: innerHeight / 2
-}
+function randomIntFromRange(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+  }
 
 const colors = ['#5ffbd6', '#7ECEFD', '#FFF6E5', '#FF0000']
 
@@ -23,7 +25,7 @@ class ball {
         this.dx = dx;
         this.dy = dy;
         this.height = height;
-        this.gravity = 0.5
+        this.gravity = 0.05
         this.color = colors[1];
         this.lastkey;
 
@@ -39,10 +41,10 @@ class ball {
         this.update = function () {
 
   
-            if (this.y  < 20 || this.y > 260){
+            if (this.y  < 50 || this.y > 290){
                 unanimate();
                 window.removeEventListener("keydown",movement);
-                
+                clearInterval(platform_generator);
             }
 
             for (var i = 0; i < platform_list.length; i++) {
@@ -96,13 +98,11 @@ class platform {
         this.draw = function () {
             c.fillStyle = colors[0];
             c.fillRect(this.x, this.y, this.width, this.height);
-            
-            c.fill();
         };
 
         this.update = function () {
             this.y += this.speed;
-            if (this.y === 20) {
+            if (this.y === 50) {
                 platform_list.splice(0, 1);
             }
             else {
@@ -114,58 +114,78 @@ class platform {
     }
 }
 
-function draw_fire(){
-    c.fillStyle = colors[3];
-    c.fillRect(0,0,canvas.width,20);
+function draw_spike(){
+    img = new Image()
+    img.src = "background/spikes.png";
+    c.drawImage(img, 0, 0, canvas.width, 50);
 }
 
 function one_platform( width = null, x = null, y = 300){
 
-    if (x === null){
-        x = Math.floor(Math.random() * canvas.width);
+    if (width === null){
+        width = randomIntFromRange(100, canvas.width/3);
     }
 
-    if (width === null){
-    width = Math.floor(Math.random() * (canvas.width - x ) );
+    if (x === null){
+        x = Math.floor(Math.random() * (canvas.width - width));
     }
+
     let single_platform = new platform( x, y, -1, width, 10);
     platform_list.push(single_platform);
-    platform_list[platform_list.length -1 ].draw();
 
 }
 
-function two_platform( x = 0, y = 305 ){
+function two_platform(width = null){
 
-    this.x = x;
-    this.y = y;
-    if (this.x === 0){
-        this.x = Math.floor(Math.random() * canvas.width);
+    if (width === null){ 
+        width = randomIntFromRange(80, canvas.width/3.5);
     }
-    single_platform = new platform( this.x, this.y, -1, 250, 10);
-    platform_list.push(single_platform);
-    
+    let x_platform = (canvas.width - (2*width))/3;
+    one_platform( width, x_platform, 300);
+    one_platform( width, (2*x_platform) + width, 300);
 
 }
 
+function three_platform(width = null){
+
+    if (width === null){ 
+        width = randomIntFromRange(70, canvas.width/5);
+    }
+    let x_platform = (canvas.width - (3*width))/4;
+    one_platform( width, x_platform, 300);
+    one_platform( width, (2*x_platform) + width, 300);
+    one_platform( width, (3*x_platform) + (2*width), 300);
+
+}
 
 function movement(e){
     moving_ball.move(e);
 }
 
+function platformpicker(){
+    number = randomIntFromRange(0,2)
+    if ( number === 0) {one_platform();}
+    else if (number === 1) {two_platform();}
+    else {three_platform();}
+}
 
+function cross_roads(){
+    if (outcome) {}
+}
 
 function init() {
-    moving_ball = new ball(canvas.width/2, 300 -50,0,1,10);
-    draw_fire();    
+    moving_ball = new ball(canvas.width/2 - 5, 300 -50,0,1,10);
+    
     window.addEventListener("keydown",movement);
-    one_platform(250 ,canvas.width/2 +10-125 , 300 -50+10 );
-    one_platform( 250);
-
+    one_platform(250 ,canvas.width/2-125 , 300 -50+10 );
+    two_platform();
+    platform_generator = setInterval( platformpicker, 1000);
     for (var i=0; i<platform_list.length; i++) {
         platform_list[i].draw();
     }
-
+    draw_spike();    
 }
+
 
 // Animation Loop
 function animate() {
@@ -173,19 +193,24 @@ function animate() {
     animate_func = requestAnimationFrame(animate);
     c.clearRect(0, 0, canvas.width, canvas.height);
     moving_ball.update();
-    draw_fire();
     moving_ball.dx = 0;
-
     for (var i=0; i<platform_list.length; i++) {
         platform_list[i].update();
     }
+    draw_spike();
 }
 
 function unanimate(){
     cancelAnimationFrame(animate_func);
 }
 
+function start() {
+    hearts = 3;
+    play_but.addEventListener('click',function(){
+        init();
+        animate();
+    });
+}
 
+start();
 
-init();
-animate();
